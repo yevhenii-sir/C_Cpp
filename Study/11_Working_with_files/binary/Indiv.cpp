f29c5de4449a2bfa8adb446d8ad94b68c49af84c
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-02-10 19:10:22 +0200

    Up

--- Study/11_Working_with_files/binary/Indiv.cpp
+++ Study/11_Working_with_files/binary/Indiv.cpp
@@ -3,29 +3,32 @@
 #include <conio.h>
 #include <string.h>
 #define maxlenght 90
-//код покищо не дописаний
+ 
 int main()
 {
     FILE *in, *out;
-    char name[25], s[maxlenght], m[maxlenght];
+    static char name[25], s[maxlenght], m[maxlenght];
     char *p;
     printf("Vvedit imya failu: ");
     gets(name);
-    if ((in = fopen(name, "r+")) == NULL) {
+    if ((in = fopen(name, "r")) == NULL) {
         printf("File %s is absent!!! \n", name);
         return 0;
     }
     printf("Vvedit imya failu dlya zberezhenya: ");
     gets(name);
-    out = fopen(name, "w");
+	out = fopen(name, "w");
     while (!feof(in)) {
         fgets(s, maxlenght, in);
-        p = strstr(s, "//");
+        p = strstr(s, "//"); //визначається входження символів "//" в строку
         if (p != NULL) {
-        //printf("%d\n", p-s+1);
-        strcpy(m, s+(p-s));
-        fprintf(out, "%s", m);
-        }
+            strncpy(m, s, (p-s)); //(p-s) - довжина строки до входження символів "//"
+            fprintf(out, "%s\n", m); //Вивод зміненої строки
+        } else {
+        	fprintf(out, "%s", s);//Вивод в тому випадку якщо не найдено символів "//"
+		}
     }
+    fclose(in);
+    fclose(out);
     return 0;
 }
