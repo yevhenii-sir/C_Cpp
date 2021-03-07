4ccc7f47c061b3a4d8afe4b4f057a0e4bd52daf0
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-03-07 21:39:33 +0200

    Add Polimorphizm

---
+++ Study/C++/5_Polymorphizm_Virtual_Override/Gun_abstract.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+class Weapon {
+public:
+    virtual void Shoot() = 0;
+};
+
+class Gun : public Weapon{
+public:
+    void Shoot() override
+    {
+        std::cout << "BANG!" << std::endl;
+    }
+};
+
+class SubmachineGun : public Gun {
+public:
+    void Shoot() override
+    {
+        std::cout << "BANG! BANG! BANG! BANG!" << std::endl;
+    }
+};
+
+class Bazooka : public Weapon {
+public:
+    void Shoot() override
+    {
+        std::cout << "BADABOOOM!" << std::endl;
+    }
+};
+
+class Knife : public Weapon {
+public:
+    void Shoot() override
+    {
+        std::cout << "VZHUH!" << std::endl;
+    }
+};
+
+class Player {
+public:
+    void Shoot(Weapon* weapon)
+    {
+        weapon->Shoot();
+    }
+};
+
+int main()
+{
+    Gun gun;
+    SubmachineGun submachGun;
+    Bazooka bazooGun;
+    Knife knife;
+    Player player;
+
+    player.Shoot(&gun);
+    player.Shoot(&submachGun);
+    player.Shoot(&bazooGun);
+    player.Shoot(&knife);
+}
---
+++ Study/C++/5_Polymorphizm_Virtual_Override/Gun_not_abstract.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+class Gun {
+public:
+    virtual void Shoot()
+    {
+        std::cout << "BANG!" << std::endl;
+    }
+};
+
+class SubmachineGun : public Gun {
+public:
+    void Shoot() override
+    {
+        std::cout << "BANG! BANG! BANG! BANG!" << std::endl;
+    }
+};
+
+class Bazooka : public Gun {
+public:
+    void Shoot() override
+    {
+        std::cout << "BADABOOOM!" << std::endl;
+    }
+};
+
+class Player {
+public:
+    void Shoot(Gun* gun)
+    {
+        gun->Shoot();
+    }
+};
+
+int main()
+{
+    Gun gun;
+    SubmachineGun submachGun;
+    Bazooka bazooGun;
+    Player player;
+
+    player.Shoot(&gun);
+    player.Shoot(&submachGun);
+    player.Shoot(&bazooGun);
+}
