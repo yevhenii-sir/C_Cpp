9200bc769fdd3fc290447d2d41a105ced5ee49a1
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-03-07 15:23:32 +0200

    Inheritance

---
+++ Study/C++/4_Inheritance/modifiers_class_inheritance.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using std::string;
+
+class A {
+public:
+	string msgOne = "One";
+private:
+	string msgTwo = "Two";
+protected:
+	string msgThree = "Three";
+};
+
+class B : public A{
+public:
+	void Show() {
+		std::cout << "1 - " << msgOne << ";\n2 - not avaliable;\n3 - " << msgThree << ';' << std::endl;
+	}
+};
+
+int main()
+{
+	B b;
+	b.Show();
+}
---
+++ Study/C++/4_Inheritance/test_class_inheritance.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using std::string;
+
+class Car {
+public:
+	string car_name;
+	string engine;
+	void Show_c()
+	{
+		std::cout << "Name: " << car_name << ";\nEngine: " << engine << ";\n";
+	}
+};
+
+class Competition_cars : public Car {
+public:
+	string power_structures;
+	void Show()
+	{
+		Show_c();
+		std::cout << "Power structures: " << power_structures << std::endl;
+	}
+};
+
+class Old_cars : public Car {
+public:
+	short int numb_of_wheels;
+	void Show()
+	{
+		Show_c();
+		std::cout << "Nummber of wheels: " << numb_of_wheels << std::endl;
+	}
+};
+
+int main()
+{
+	Competition_cars comptCar1;
+	comptCar1.car_name = "Peugot 206 WRC";
+	comptCar1.engine = "PSA XU9J4";
+	comptCar1.power_structures = "Steel monocoque with roll-cage.";
+	comptCar1.Show();
+	std::cout << std::endl;
+	Old_cars oldCar1;
+	oldCar1.car_name = "Velorex";
+	oldCar1.engine = "250-cc JWA motor";
+	oldCar1.numb_of_wheels = 3;
+	oldCar1.Show();
+}
