e32a35e659bcf5bd983adc78825912a640aa660c
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-03-11 21:45:04 +0200

    Added work with dynamic memory

---
+++ Study/C++/8_Heap(new_delete)/Test_new_delete_nullptr.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int* pt = new int;
+	*pt = 10;
+
+	std::cout << *pt << std::endl;
+
+	delete pt;
+
+	pt = nullptr;
+
+	if (pt != nullptr)
+	{
+		std::cout << pt << std::endl;
+	}
+}
