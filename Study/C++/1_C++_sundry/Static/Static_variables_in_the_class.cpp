bcbc9b7dc7f722ad3771bc1b3f43ea7e1b29e031
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-04-19 22:38:15 +0300

    fix incaps

--- Study/C++/1_C++_sundry/Static/Static_variables_in_the_class.cpp
+++ Study/C++/1_C++_sundry/Static/Static_variables_in_the_class.cpp
@@ -5,15 +5,18 @@ using namespace std;
 class Apple {
 	int weight;
 	string color;
-public:
 	static int count;
+public:
 	Apple() { ++count; }
 	Apple(int _weight, string _color) {
 		weight = _weight;
 		color = _color;
 		++count;
 	}
-	void SetCount(int x) { count = x; }
+	static int GetCount() {
+		return count;
+	}
+	static void SetCount(int x) { count = x; }
 };
 
 int Apple::count = 0;
@@ -21,7 +24,7 @@ int Apple::count = 0;
 int main()
 {
 	Apple a, b(2, "orange"), c;
-	cout << Apple::count << endl;
-	Apple::count = 10;
-	cout << Apple::count << endl;
+	cout << Apple::GetCount() << endl;
+	Apple::SetCount(10);
+	cout << Apple::GetCount() << endl;
 }
