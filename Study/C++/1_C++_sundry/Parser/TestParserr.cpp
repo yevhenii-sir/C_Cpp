936aedb90d4a03aec30a7fdb37831f7aae5b153b
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-19 21:26:31 +0300

    Add dictionary parser

---
+++ Study/C++/1_C++_sundry/Parser/TestParserr.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <clocale>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    ifstream fileinp;
+    fileinp.open("index.dic");
+
+    ofstream fileoutp;
+    fileoutp.open("outpDictionary.dic");
+
+    string input_string;
+    int pos;
+
+    while (!fileinp.eof()) {
+        fileinp >> input_string;
+        pos = input_string.find("/");
+        input_string.resize((pos == -1) ? input_string.length() : pos);
+        fileoutp << input_string << endl;
+    }
+
+    return 0;
+}
